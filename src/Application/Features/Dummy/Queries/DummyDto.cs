a2fc5dc2d3d953e650fb600dbce1e6bb412dec1c
Rolf Kempf <rolf.kempf@meta-level.de>
2025-04-09 21:56:04 +0200

    Add EditorConfig file and update VSCode settings for consistent formatting

--- src/Application/Features/Dummy/Queries/DummyDto.cs
+++ src/Application/Features/Dummy/Queries/DummyDto.cs
@@ -3,7 +3,9 @@ namespace Application.Features.Dummy.Queries
     public class DummyDto
     {
         public string Name { get; set; }
+
         public string Gender { get; set; }
+
         public int Age { get; set; } // Calculated from Birthday
 
         public DummyDto(string name, string gender, int age)
